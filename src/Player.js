ce9c65882883a02f75b909d581315e31776e8e6c
Antonio Muniz <antonio.gustavo_@live.com>
2022-06-18 21:57:58 -0300

    Added more effects

--- src/Player.js
+++ src/Player.js
@@ -12,10 +12,22 @@ import atumalaca from './songs/atumalaca.mp3'
 import chega from './songs/chegaa.mp3'
 import dancagatinho from './songs/dancagatinho.mp3'
 import demais from './songs/demais.mp3'
+import elegosta from './songs/elegosta.mp3'
+import uii from './songs/uii.mp3'
+import queissomeufilhocalma from './songs/queissomeufilhocalma.mp3'
+import olhaeleae from './songs/olhaeleae.mp3'
+import ebrincadeirahein from './songs/ebrincadeirahein.mp3'
+import okay from './songs/okayokay.mp3'
+import pareoriginal from './songs/pareoriginal.mp3'
+import uh from './songs/uh.mp3'
+import irra from './songs/irra.mp3'
+import nao from './songs/nao.mp3'
+import decepcao from './songs/triste.mp3'
+import quepapelao from './songs/quepapelaohein.mp3'
+import esquecatudo from './songs/esquecatudo.mp3'
 
 function Player ( { name, imgUri, song } ) {
     const [playSound] = useSound(song)
-    console.log(demais)
 
     return (
         <button className="order-card-container" onClick={playSound}>
